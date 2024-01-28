da79b41a64d234ed6a4b2b5777ec1a3358e4b17f
michael69zen <145418799+michael69zen@users.noreply.github.com>
2024-01-28 04:31:41 -0500

    <3

---
+++ 23_omegaUp_10771.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Entrada de la letra
+    char letra;
+    cout << "Ingrese una letra: ";
+    cin >> letra;
+
+    // Imprimir el corazón
+    cout << "      " << letra << "      \n";
+    cout << "   " << letra << "     " << letra << "   \n";
+    cout << " " << letra << "         " << letra << " \n";
+    cout << "   " << letra << "     " << letra << "   \n";
+    cout << "      " << letra << "      \n";
+    cout << "        " << letra << "        \n";
+    cout << "          " << letra << "          \n";
+    cout << "            " << letra << "            \n";
+    cout << "              " << letra << "              \n";
+    cout << "                " << letra << "                \n";
+    cout << "                  " << letra << "                  \n";
+    cout << "                    " << letra << "                    \n";
+
+    return 0;
+}
