a05be2d21581447651cd3643f087b606066ff347
michael69zen <145418799+michael69zen@users.noreply.github.com>
2024-01-29 06:17:48 -0500

    un cubo / 2

---
+++ 89_omegaUp_7139.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Leer el número entero
+    int n;
+    cin >> n;
+
+    // Calcular el resultado
+    int resultado = (n * n * n) / 2;
+
+    // Mostrar el resultado
+    cout << resultado << endl;
+
+    return 0;
+}
